dee9b65924dbb1c81c417b76b0545f9c508aafa4
Exinker <exinker@gmail.com>
2023-05-13 14:07:58 +0700

    init: init commit

---
+++ pyrometer/core/config.py
@@ -0,0 +1,14 @@
+
+# ---------        CONSTANTS        ---------
+DEBUG = True
+AUTOAPPLY = True
+AUTOCALCULATE = True
+
+# ---------        CONTACTS        ---------
+ORGANIZATION_NAME = 'VMK-Optoelektronika'
+
+APPLICATION_NAME = 'Spectral Pyrometer'
+APPLICATION_VERSION = '0.0.1'
+
+AUTHOR_NAME = 'Pavel Vaschenko'
+AUTHOR_EMAIL = 'vaschenko@vmk.ru'
---
+++ setup.py
@@ -0,0 +1,39 @@
+
+import os
+from setuptools import setup, find_packages
+
+
+from pyrometer import APPLICATION_VERSION, AUTHOR_NAME, AUTHOR_EMAIL
+
+
+#
+setup(
+    # info
+    name='pyrometer',
+    description='Specral pyrometer to estimate temperature of heated body.',
+    license='MIT',
+
+    # version
+    version=APPLICATION_VERSION,
+
+    # author details
+    author=AUTHOR_NAME,
+    author_email=AUTHOR_EMAIL,
+
+    # setup directories
+    packages=find_packages(),
+    # package_dir={
+    #     '': 'pyrometer',
+    # },
+    # package_data={
+    #     '': [],
+    # },
+
+    # requires
+    install_requires=['PySide6', 'spectrumlab @ git+https://github.com/Exinker/spectrumlab.git'],
+    python_requires='>=3.10',
+
+    dependency_links=[
+        r'//10.11.12.11/Users/vaschenko/spectrumlab/dist/',
+    ]
+)
